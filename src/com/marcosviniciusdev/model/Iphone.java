3c2ebf1e1a801548b03e7f60fc5f8388a6bda884
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-12-02 18:20:16 -0300

    feat: create the constructor using all attributes

--- src/com/marcosviniciusdev/model/Iphone.java
+++ src/com/marcosviniciusdev/model/Iphone.java
@@ -5,6 +5,11 @@ public class Iphone {
     private String cor;
     private int memoria;
 
+    public Iphone(String cor, int memoria) {
+        this.cor = cor;
+        this.memoria = memoria;
+    }
+
     public String getCor() {
         return cor;
     }
