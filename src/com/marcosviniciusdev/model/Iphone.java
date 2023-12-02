3e31e0f30a275e8352898327ae8bd82986ba3402
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-12-02 20:55:26 -0300

    feat: add news attributes, new getter and new features in the methods.

--- src/com/marcosviniciusdev/model/Iphone.java
+++ src/com/marcosviniciusdev/model/Iphone.java
@@ -4,6 +4,8 @@ import com.marcosviniciusdev.interfaces.AparelhoTelefonico;
 import com.marcosviniciusdev.interfaces.NavegadorInternet;
 import com.marcosviniciusdev.interfaces.ReprodutorMusical;
 
+import java.util.List;
+
 public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
 
     private String cor;
@@ -13,6 +15,8 @@ public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorI
     private boolean isLigado;
     private boolean isChamada;
     private boolean isCorreioVoz;
+    private String paginaAtual;
+    private List<String> paginas;
 
     public String getCor() {
         return cor;
@@ -70,48 +74,83 @@ public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorI
         isCorreioVoz = correioVoz;
     }
 
+    public String getPaginaAtual() {
+        return paginaAtual;
+    }
+
     @Override
     public void ligar() {
-
+        if (!isLigado()) {
+            this.setLigado(true);
+            System.out.println("O Iphone foi ligado.");
+        } else {
+            this.setLigado(false);
+            System.out.println("O Iphone foi desligado.");
+        }
     }
 
     @Override
     public void atender() {
-
+        if (!isChamada) {
+            this.setChamada(true);
+            System.out.println("A chamada foi atendida.");
+        } else {
+            this.setChamada(false);
+            System.out.println("A chamada foi desligada.");
+        }
     }
 
     @Override
     public void iniciarCorreioVoz() {
-
+        if (!isCorreioVoz()) {
+            this.setCorreioVoz(true);
+            System.out.println("O correio de voz foi iniciado.");
+        } else {
+            this.setCorreioVoz(false);
+            System.out.println("O correio de voz foi finalizado.");
+        }
     }
 
     @Override
     public void exibirPagina() {
-
+        if (!paginaAtual.isEmpty()){
+            System.out.println("A pagina atual é " + this.getPaginaAtual());
+        } else {
+            System.out.println("Não há nenhuma pagina aberta.");
+        }
     }
 
     @Override
-    public void adicionarNovaAba() {
-
+    public void adicionarNovaAba(String pagina) {
+        paginas.add(pagina);
     }
 
     @Override
     public void atualizarPagina() {
-
+        System.out.println("A pagina " + this.getPaginaAtual() + " foi atualizada.");
     }
 
     @Override
     public void tocar() {
-
+        if (!isReproduzindoMusica()) {
+            System.out.println("A musica começou a tocar.");
+        } else {
+            System.out.println("A musica já está tocando.");
+        }
     }
 
     @Override
     public void pausar() {
-
+        if (isReproduzindoMusica) {
+            System.out.println("A musica foi pausada.");
+        } else {
+            System.out.println("A musica já está em pausa.");
+        }
     }
 
     @Override
-    public void selecionarMusica() {
-
+    public void selecionarMusica(String nomeMusica) {
+        this.setNomeMusica(nomeMusica);
+        System.out.println("A musica " + this.getNomeMusica() + " foi selecionada.");
     }
 }
