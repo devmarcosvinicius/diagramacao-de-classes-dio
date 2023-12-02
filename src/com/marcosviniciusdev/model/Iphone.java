7c6d068f81bed8f6c73d19f9ca6a361b88960f10
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-12-02 18:19:39 -0300

    feat: create attributes and getters and setters

---
+++ src/com/marcosviniciusdev/model/Iphone.java
@@ -0,0 +1,23 @@
+package com.marcosviniciusdev.model;
+
+public class Iphone {
+
+    private String cor;
+    private int memoria;
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getMemoria() {
+        return memoria;
+    }
+
+    public void setMemoria(int memoria) {
+        this.memoria = memoria;
+    }
+}
